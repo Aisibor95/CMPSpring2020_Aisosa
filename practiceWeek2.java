1669ee20a1c020952a2a0bb5f4258314e1be5d20
Aisibor95 <60441050+Aisibor95@users.noreply.github.com>
2020-02-03 12:41:04 -0500

    Add files via upload

---
+++ practiceWeek2.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @author Aisosa
+ * created:2/3/20
+ *description: Variable Practice
+ */
+public class practiceWeek2 {
+       //Define a variable of type 
+		double money = 10.35;
+		byte candy = 5;
+		int range = 3;
+	
+	
+	
+	
+	
+
+  public static void main(String[] args) {
+	//Define a variable of type 
+			double money = 10.35;
+			byte candy = 5;
+			int range = 3;
+	  
+	  
+	//Declaring 3 String variables
+	     String name1;
+		 String name2;
+		 String name3;
+	  
+		//Assigning values to the previously created variables
+	    name1= "Matt";
+		name2="genesis";
+		name3="karina";
+		
+		//printing
+		System.out.println(name1);
+		System.out.println(name1+name3);//Expected
+  }
+}
