3541224e39350657d032bb6fba42ee5bf6caa424
Aisibor95 <60441050+Aisibor95@users.noreply.github.com>
2020-02-05 12:42:15 -0500

    Add files via upload

---
+++ classwork.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Aisosa
+ * created:2/5/20
+ *description: Numbers
+ */
+
+package practice2of2;
+
+import java.util.Scanner;
+
+public class classwork {
+	public static void main(String[] args) {
+       
+		int x, y, z;
+		System.out.println("Enter three integers");
+		Scanner in = new Scanner(system.in);
+		
+		x = in.nextInt();
+		y = in.nextInt();
+		z = in.nextInt();		
+      
+}
+}
